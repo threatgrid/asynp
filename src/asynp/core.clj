5c59336e2df74b9e6e012362e3c16a80c454234a
Charles Duffy <charles@threatgrid.com>
2014-04-09 16:55:46 -0500

    Respond to review feedback; implement (most) fixups suggested by kibit

--- src/asynp/core.clj
+++ src/asynp/core.clj
@@ -1,24 +1,32 @@
 (ns asynp.core
   (:require [clojure.core.async :as async :refer [<! >! <!! >!! timeout chan alts! alt!! alts!! go close!]]
+            [clojure.string]
             [taoensso.timbre :as timbre])
   (:import [com.zaxxer.nuprocess NuProcess NuProcessBuilder NuProcessHandler]
            [java.nio ByteBuffer CharBuffer]
            [java.nio.charset Charset CoderResult]
            [java.util.regex Pattern]))
 
-;; core.async makes avoiding reflection usually difficult -- hints inside go
-;; blocks are frequently ineffective.
-(set! *warn-on-reflection* true)
-
 (def ^:dynamic *working-buffer-size* 1024)
 
 (defn array-from-buffer [^ByteBuffer buffer]
+  "Given a ByteBuffer, return an array of bytes"
   (let [byte-count (.remaining buffer)
         dest (byte-array byte-count)]
     (.get buffer dest)
     dest))
 
 (defn run-process [argv]
+  "Run a process with the given argv, and return a dictionary with access to the process object itself and channels for controlling it.
+
+  Returns a dictionary with the following keys:
+    :process - a NuProcess object
+    :in - an unbuffered channel used for input. Contents will be streamed to the process's stdin.
+    :out - an unbuffered channel used for output. Contains contents read from the process's stdout.
+    :err - an unbuffered channel used for output. Contains contents read from the process's stderr.
+    :exit - a buffered channel used for output. Empty until process has exited. Contains exit status, or INT_MIN if process was not successfully invoked.
+
+  All objects on the :in, :out or :err channels should be ByteBuffer instances positioned for reading."
   (let [in-chan (chan), out-chan (chan), err-chan (chan), exit-chan (chan 1)
         process-atom (atom nil)]
     (let [^NuProcessHandler handler
@@ -86,7 +94,7 @@
 
                 (.flush decoder ^CharBuffer out-buffer)
                 (.flip out-buffer)
-                (when (> (.remaining out-buffer) 0)
+                (when (pos? (.remaining out-buffer))
                   (>! out-chan out-buffer))
                 (close! out-chan))
               nil)
@@ -143,7 +151,7 @@
         (timbre/trace "Buffered:" (pr-str strings-without-delim))
         (timbre/trace "Handling:" (pr-str input-charbuf))
         (if input-charbuf
-          (let [input-str (.toString ^CharBuffer input-charbuf)
+          (let [input-str (str input-charbuf)
                 pieces (vec (.split pattern input-str -1))]
             (timbre/trace "Split string" (pr-str input-str)
                           "into pieces" (pr-str pieces)
@@ -154,7 +162,7 @@
                 (recur (conj strings-without-delim input-str) (<! in-chan)))
               (do
                 ;; first piece is appended to queued strings
-                (>! out-chan (apply str (conj strings-without-delim (first pieces))))
+                (>! out-chan (clojure.string/join (conj strings-without-delim (first pieces))))
                 ;; middle pieces go out as-is
                 (doseq [ready-string (rest (pop pieces))]
                   (>! out-chan ready-string))
@@ -163,23 +171,22 @@
                   (recur [last-piece] (<! in-chan))))))
           (do
             ;; end of stream
-            (let [last-piece (apply str strings-without-delim)]
-              (when-not (empty? last-piece)
-                (>! out-chan )))))))
+            (let [last-piece (clojure.string/join strings-without-delim)]
+              (when (seq last-piece)
+                (>! out-chan last-piece)))))))
     out-chan))
 
 (defn log-strings
+  "Debugging aid: Given a channel delivering character buffers, log the contained strings using Timbre
+
+  When given a character to split by, log only when a complete character-delimited buffer is received, or at end-of-stream."
   ([in-chan]
-     "Debugging aid: Given a channel delivering character buffers, log the contained strings using Timbre"
      (go
        (loop [char-buffer-in (<! in-chan)]
          (when char-buffer-in
-           (timbre/trace "log-strings received buffer: " char-buffer-in)
-           (let [s (.toString ^CharBuffer char-buffer-in)]
-             (timbre/info "Read string: " s))
+           (timbre/info "Read string: " (str char-buffer-in))
            (recur (<! in-chan))))))
   ([in-chan split-char]
-     "Debugging aid: Given a channel delivering character buffers, split strings by a given character and log them individually using Timbre"
      (let [str-chan (split-by-char in-chan split-char)]
        (go (loop [string-in (<! str-chan)]
              (timbre/info "Read string: " string-in)
@@ -187,6 +194,7 @@
 
 
 (defn write-str-to-process [proc-dict, ^String s]
+  "Given a process dictionary, queue content to be written to its stdin"
   (>!! (:in proc-dict) (.getBytes s)))
 
 (defn close-stdin-for-process [proc-dict]
@@ -196,11 +204,10 @@
   (close! (:in proc-dict)))
 
 (defn wait-for-process
+  "Block until a process exits, or an optional timeout occurs; return exit status, or nil for timeout"
   ([proc-dict]
-     "Block until a process exits"
      (<!! (:exit proc-dict)))
   ([proc-dict timeout-ms]
-     "Wait for up to timeout-ms for process to exit; return exit status if it does, nil if it does not"
      (let [exit-chan {:exit proc-dict}
            [c v] (alts!! [exit-chan (timeout timeout-ms)])]
        (when (= c exit-chan)
