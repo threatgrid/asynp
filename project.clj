6b9ec086f0029294e18d81663f2941bc93eca5f6
Charles Duffy <charles@threatgrid.com>
2014-04-09 20:04:07 -0500

    Prepare for 0.0.3 development series

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject asynp "0.0.2"
+(defproject asynp "0.0.3-SNAPSHOT"
   :description "A core.async library for efficient subprocess management"
   :url "http://github.com/threatgrid/asynp"
   :license {:name "Eclipse Public License"
