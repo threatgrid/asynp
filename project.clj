ad0a622d9fd1e091b1740ae13aaec633ec7f3fdc
Charles Duffy <charles@threatgrid.com>
2014-04-09 19:00:41 -0500

    configure for use with lein-release plugin

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://github.com/threatgrid/asynp"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :lein-release {:deploy-via :clojars}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/core.async "0.1.278.0-76b25b-alpha"]
                  [com.taoensso/timbre "3.1.6"]
