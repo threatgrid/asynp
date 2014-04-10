022ba984f48bcba0964559d1582e247c935a87d6
Charles Duffy <charles@threatgrid.com>
2014-04-09 19:35:54 -0500

    release against nuprocess 0.9.1

--- project.clj
+++ project.clj
@@ -7,4 +7,5 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/core.async "0.1.278.0-76b25b-alpha"]
                  [com.taoensso/timbre "3.1.6"]
-                 [com.zaxxer/nuprocess "0.9.2-SNAPSHOT"]])
+                 [com.zaxxer/nuprocess "0.9.1"] ; note: when released, 0.9.2 will be significantly preferable due to a pending bugfix
+                 ])
