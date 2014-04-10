c5d9c6aff38808325f434d3eed1c17214869a036
Charles Duffy <charles@threatgrid.com>
2014-04-09 20:01:07 -0500

    ditch lein-release (which lacks GPG support) and prepare for a signed 0.0.2 release

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
-(defproject asynp "0.0.2-SNAPSHOT"
+(defproject asynp "0.0.2"
   :description "A core.async library for efficient subprocess management"
   :url "http://github.com/threatgrid/asynp"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :scm {:name "git" :url "https://github.com/threatgrid/asynp"}
-  :lein-release {:deploy-via :clojars}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/core.async "0.1.278.0-76b25b-alpha"]
                  [com.taoensso/timbre "3.1.6"]
